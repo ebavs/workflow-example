997017b021ef9f7a5f6828bb4059ce0fa23f66da
victor <victor@ebavs.net>
2016-12-14 16:54:22 +0100

    Created index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$msg = 'Victor was here';
+?>
+<html>
+<head>
+	<title>Git WorkFlow Example</title>
+</head>
+<body>
+<p>The train goes forward unstoppable</p>
+<p><?php echo $msg; ?>
+<body>
+</html>
